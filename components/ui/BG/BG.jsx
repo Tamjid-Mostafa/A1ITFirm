7d65f3fcf581b5c5d84aacb5c242dc565dc0ef8c
Tamjid Mostafa <tamjid430@gmail.com>
2023-04-03 03:14:47 +0600

    bg image

--- components/ui/BG/BG.jsx
+++ components/ui/BG/BG.jsx
@@ -1,6 +1,21 @@
 import Image from 'next/image'
 import React from 'react'
 const BG = ({bg}) => {
+    // Pixel GIF code adapted from https://stackoverflow.com/a/33919020/266535
+const keyStr =
+'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
+const triplet = (e1, e2, e3) =>
+keyStr.charAt(e1 >> 2) +
+keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+keyStr.charAt(e3 & 63)
+
+const rgbDataURL = (r, g, b) =>
+`data:image/gif;base64,R0lGODlhAQABAPAA${
+  triplet(0, r, g) + triplet(b, 255, 255)
+}/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
+
     return (
         <div
                 style={{
@@ -20,7 +35,7 @@ const BG = ({bg}) => {
                     style={{
                         objectFit: 'cover',
                     }}
-                    blurDataURL={bg}
+                    blurDataURL={rgbDataURL(2, 129, 210)}
                     alt=""
                 />
             </div>
